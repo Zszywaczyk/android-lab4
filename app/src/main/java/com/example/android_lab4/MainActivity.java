a4dd4e8d8c02792f373d4f56163e04cbc77de8d2
a-Zszywacz <Teresher935@GMail.com>
2019-04-07 23:17:12 +0200

    Lab5 done. Added long click listener that delete list items.

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -53,6 +53,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intencja, 2);
             }
         });
+        listView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        TextView name = view.findViewById(android.R.id.text1);
+                        db.usun(name.getText().toString());
+                        adapter.changeCursor(db.lista());
+                        adapter.notifyDataSetChanged();
+                        return true;
+                    }
+                }
+        );
 
     }
 
